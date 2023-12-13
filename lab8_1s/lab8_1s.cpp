fa4480b21679357366e60257ae9614939234b96e
Newstar12321 <tazeev.zhenya@mail.ru>
2023-12-13 15:31:48 +0700

    Добавьте файлы проекта.

---
+++ lab8_1s/lab8_1s.cpp
@@ -0,0 +1,31 @@
+﻿/*
+Даны строки S и S0. Проверить, содержится ли строка S0 в строке S.
+Если содержится, то вывести TRUE, если не содержится, то вывести FALSE.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+    string str;
+    cout << "Введите строку s:\n";
+    getline(cin,str);
+
+    string str0;
+    cout << "Введите строку s0:\n";
+    getline(cin, str0);
+
+    if (str.find(str0, 0) != string::npos)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+}
